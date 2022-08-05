e80873d56d4137a6d7c48e66e81400593c51768c
fryzahh <91803859+fryzahh@users.noreply.github.com>
2022-08-04 23:46:03 -0400

    Added many more utility and helper files to Common

---
+++ Common/ILEditing.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Insanity.Common.Systems.ParticleSystem;
+
+namespace Insanity.Common
+{
+    public static class ILEditing
+    {
+        public static void Initialize()
+        {
+            On.Terraria.Main.DrawInterface += DrawParticles;
+        }
+
+        public static void Unload()
+        {
+            On.Terraria.Main.DrawInterface -= DrawParticles;
+        }
+
+        private static void DrawParticles(On.Terraria.Main.orig_DrawInterface orig, Main self, GameTime gameTime)
+        {
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, default, default, RasterizerState.CullNone, default, Main.GameViewMatrix.ZoomMatrix);
+            ParticleManager.DrawParticles(Main.spriteBatch);
+            Main.spriteBatch.End();
+
+            orig(self, gameTime);
+        }
+    }
+}
---
+++ Common/Utilities/BossHelper.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+using System;
+using System.IO;
+
+namespace Insanity.Common.Utilities
+{
+    public static class BossHelper 
+    {
+		
+	}
+}
